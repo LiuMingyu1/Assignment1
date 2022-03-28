e24634b2f15a42a1e28508a5c7ff5fe29667e676
LiuMingyu1 <102500314+LiuMingyu1@users.noreply.github.com>
2022-03-28 13:08:46 +0800

    Add files via upload

---
+++ Student.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: SELECT * FROM db1.student
+LIMIT 0, 1000
+
+-- Date: 2022-03-28 13:08
+*/
+INSERT INTO `` (`idStudent`,`StudentName`,`department`) VALUES (200019,'LIU MINGYU','Software engineering');
+INSERT INTO `` (`idStudent`,`StudentName`,`department`) VALUES (201510,'Chang Feng','Software engineering');
+INSERT INTO `` (`idStudent`,`StudentName`,`department`) VALUES (201526,'Wang Dong','Computer Science');
